77723c2851a002e7ca011095ffa61b27ac53ea2a
Mutian Ling <mutian@Mutians-Air.lan>
2022-11-26 18:03:50 -0500

    backend start

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,24 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getbotinfo")
+    public Map<String,String> getBotInfo(){
+            Map<String,String> m = new HashMap<>();
+            m.put("name","tiger");
+            m.put("rating","1500");
+            return m;
+
+    }
+
+}
